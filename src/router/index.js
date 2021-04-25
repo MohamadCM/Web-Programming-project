cd7a03acf123562dcd54361272a338d370f19f89
Mohamad Chaman-Motlagh <m.chamanmotlagh@gmail.com>
2021-04-25 19:38:46 +0430

    Create tab selector

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
+import Profile from "../views/Profile";
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
 		path: "/login",
 		name: "ورود",
 		component: Login
+	},
+	{
+		path: "/profile",
+		name: "پروفایل کاربری",
+		component: Profile
 	}
 	/* {
     path: '/about',
---
+++ src/views/Profile.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <tab-selector
+      :tabs="tabs"
+    />
+  </div>
+</template>
+
+<script>
+import tabSelector from "../components/core/tab-selector";
+
+export default {
+	name: "Profile",
+	components: {
+	  tabSelector
+	},
+	data(){
+	  return {
+			tabs: [
+				{name: "پروفایل"},
+				{name: "رسیدها"},
+				{name: "ویرایش"}
+			]
+		};
+	}
+};
+</script>
+
+<style scoped>
+
+</style>
