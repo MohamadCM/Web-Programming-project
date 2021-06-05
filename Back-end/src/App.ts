d76647d10d257790b108f878c76eb68632c5fdf7
Mohamad Chaman-Motlagh <m.chamanmotlagh@gmail.com>
2021-06-05 13:20:38 +0430

    Add back-end from github.com/MohamadCM/Node.js-with-Express-and-Typescript-and-Mongoose-Template

---
+++ Back-end/src/App.ts
@@ -0,0 +1,26 @@
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+import {Database} from "../config/Database";
+
+//importing route files
+import auth from "./routes/api/users/auth";
+
+const app: Application = express();
+const port: number = 5000 || process.env.PORT;
+//Connect to database
+Database.getInstance();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello!\n this is Web programming project!");
+});
+
+// Starting sever
+app.listen(port, () => {
+    console.log("\x1b[32m", "\x1b[4m", `Server running on ${port}`, "\x1b[0m");
+});
+
+//Routes
+app.use("/api/users/auth", auth);
