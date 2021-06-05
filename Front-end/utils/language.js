29dff44ec6fe252b26f7aaf4d1f8664dc2056f70
Mohamad Chaman-Motlagh <m.chamanmotlagh@gmail.com>
2021-06-05 12:55:32 +0430

    Move front-end code to front-end folder

---
+++ Front-end/utils/language.js
@@ -0,0 +1,10 @@
+function toFarsiNumber(n) {
+	const farsiDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+
+	return n
+		.toString()
+		.split("")
+		.map((x) => (Number(x) || x === "0" ? farsiDigits[x] : x))
+		.join("");
+}
+export default { toFarsiNumber };
