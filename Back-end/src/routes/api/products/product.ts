6253893b6671f9952bf77301949523a0b921918a
Mohamad Chaman-Motlagh <m.chamanmotlagh@gmail.com>
2021-06-13 13:32:09 +0430

    Add POST product API

--- Back-end/src/routes/api/products/product.ts
+++ Back-end/src/routes/api/products/product.ts
@@ -2,6 +2,11 @@ import express, {Request, Response, Router} from "express";
 import messages from "../../../config/Messages";
 import {Product} from "../../../models/Product";
 import {logError} from "../../../config/Logger";
+import authorize from "../../../middlewares/authorization";
+import {Admin} from "../../../models/Admin";
+import Constants from "../../../config/Constants";
+import {DBResponse} from "../../../interface/Database";
+import {Category} from "../../../models/Category";
 
 const router: Router = express.Router();
 
@@ -42,4 +47,58 @@ router.get("/", async (req: Request, res: Response) => {
 	}
 });
 
+//  @route  POST api/products/
+//  @decs   add or update a product
+//  @access admin users
+router.post("/", authorize(new Admin(Constants.UNKNOWN, Constants.UNKNOWN)),
+	async (req: Request, res: Response) => {
+		try {
+			let {_name, _category, _picture, _soldCount, _price, _newName} = req.body;
+			let updateObject = {};
+			if(_name){ // Will update if name is provided
+			    const productResponse: DBResponse = await new Product(Constants.UNKNOWN).getFromDB(_name);
+			    if(!productResponse.getSuccess()) {
+					res.status(404).json({...messages.wrongInput, message: productResponse.getMessage()});
+					return;
+				}
+			    const product = <Product> productResponse.getPayload();
+			    _name = product.name;
+			    _category = _category || product.category;
+			    _picture = _picture || product.picture;
+			    _soldCount = _soldCount || product.soldCount;
+			    _price = _price || product.price;
+			    _newName = _newName || product.name;
+			}
+			if(!_category || !_price || !_newName) {
+				res.status(422)
+					.json({...messages.wrongInput, message: "Required fields are not provided"});
+				return;
+			}
+			updateObject = {_name: _newName, _category, _picture,
+				_soldCount: _soldCount ? Number.parseInt(_soldCount) : undefined,
+				_price: Number.parseInt(<string>_price)};
+			if(_category){
+			    const categoryResponse: DBResponse = await new Category(Constants.UNKNOWN).getFromDB(_category);
+			    if(!categoryResponse.getSuccess()){
+					res.status(404).json({...messages.wrongInput, message: categoryResponse.getMessage()});
+					return;
+				}
+			}
+			const product = new Product(Constants.UNKNOWN)
+				.wrap(!_name ? updateObject : {_name, _category, _picture, _soldCount, _price});
+			product.updateObject = updateObject;
+			const productResponse: DBResponse = await product.saveToDB();
+			if(productResponse.getSuccess()){
+				res.status(_name ? 200 : 201)
+					.json({...messages.success, message: productResponse.getMessage()});
+			    return;
+			}
+			res.status(400).json(messages.somethingWentWrong);
+			return;
+		} catch (e) {
+			logError(`Something went wrong during API call, Input query: ${JSON.stringify(req.body)}`, "POST products");
+			res.status(500).json(messages.somethingWentWrong);
+			return;
+		}
+	});
 export default router;
