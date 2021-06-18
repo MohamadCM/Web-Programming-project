4f70b2e1e0f095f4f6d8567d984d93c3b7e41721
Mohamad Chaman-Motlagh <m.chamanmotlagh@gmail.com>
2021-06-18 20:22:56 +0430

    Create increase credit API

--- Back-end/src/config/Constants.ts
+++ Back-end/src/config/Constants.ts
@@ -7,7 +7,8 @@ interface PARAMETERS {
 	SECRET_KEY: string,
 	HASH_SALT_ROUNDS: number,
 	ASSETS_PATH: Record<"PRODUCT_PICTURES", string>;
-	DEFAULT_CATEGORY_NAME: string
+	DEFAULT_CATEGORY_NAME: string,
+	CREDIT_INCREMENT_VALUE: number
 }
 
 const parameters: PARAMETERS = {
@@ -17,7 +18,10 @@ const parameters: PARAMETERS = {
 	ASSETS_PATH: {
 		PRODUCT_PICTURES: "public/product-pic"
 	},
-	DEFAULT_CATEGORY_NAME: process.env.DEFAULT_CATEGORY_NAME || "دسته‌بندی نشده"
+	DEFAULT_CATEGORY_NAME: process.env.DEFAULT_CATEGORY_NAME || "دسته‌بندی نشده",
+	CREDIT_INCREMENT_VALUE: Number
+		.parseInt(process.env.CREDIT_INCREMENT_VALUE && Number(process.env.CREDIT_INCREMENT_VALUE) ?
+			process.env.CREDIT_INCREMENT_VALUE : "10000")
 };
 
 export default parameters;
